8803a10c47f7e01ea02177c0b98278320d2a67ca
marcus <mv.telecom20@gmail.com>
2021-01-15 16:05:49 -0300

    Projeto Base

---
+++ android/app/src/main/kotlin/br/com/mvtelecom/controle_marcus/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.mvtelecom.controle_marcus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+  Firestore.instance.collection('teste').document('1').setData({'tjhj': 'rj'});
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: Container(),
+    );
+  }
+}
